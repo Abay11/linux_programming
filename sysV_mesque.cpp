f0a355df09df63654c0f39754d0db9257eda3f3b
J.A.R.V.I.S <adygha@J.A.R.V.I.S>
2018-08-05 13:43:05 +0300

    sysV_mesque.cpp (fixed)

--- sysV_mesque.cpp
+++ sysV_mesque.cpp
@@ -9,18 +9,17 @@
 
 int main()
 {
-    key_t key=ftok("/tmp/msg.temp", 1);
-    int		msgflg, res;
+    key_t 	key=ftok("/tmp/msg.temp", 1);
+    int		res;
     pid_t 	pid;
-    int msqid;
+    int 	msqid;
 
     struct msgbuf
     {
-        long mtype=1;       /* message type, must be > 0 */
+        long mtype;       /* message type, must be > 0 */
         char msg[80];    /* message data */
     }msgbuf;
 
-    strcpy(msgbuf.msg, "hellO");
 
     msqid=msgget(key, IPC_CREAT|0666);
     assert(msqid!=-1);
@@ -30,15 +29,16 @@ int main()
     if(!pid)
     {
         //child's part
-        res=msgrcv(msqid,&msgbuf,80,1,0);
+        res=msgrcv(msqid,&msgbuf,80,0,0);
         assert(-1!=res);
         std::cout<<msgbuf.msg<<std::endl;
     }
     else
     {
         //parent's part
+        msgbuf.mtype=1;
+        strcpy(msgbuf.msg, "hellO");
         res=msgsnd(msqid, &msgbuf, 80, 0);
         assert(-1!=res);
     }
 }
-
